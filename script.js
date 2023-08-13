40959fd18486efc301bcbf4e34b8b6741a0a33cd
vijithahmbandara <vijithahmbandara@gmail.com>
2023-08-13 22:09:18 +0530

    portfolio

---
+++ script.js
@@ -0,0 +1,18 @@
+let MenuBtn = document.getElementById('MenuBtn')
+
+MenuBtn.addEventListener('click', function(e){
+    document.querySelector('body').classList.toggle('mobile-nav-active');
+    this.classList.toggle('fa-xmark')
+}) 
+
+
+function toggleContent(contentId) {
+    
+    var content = document.getElementById(contentId);
+    if (content.style.maxHeight) {
+        content.style.maxHeight = null;
+    } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+    }
+}
+
